ad55d4423aeede05c49e07e2b59b4c5f0aaab9a3
ashwi <ashwini.channaveerappa@gmail.com>
2025-08-03 15:21:51 +0200

    feat: initial commit with Selenium4 project setup

---
+++ src/main/java/practice_sessions/DataTypes/DataTypesConcept.java
@@ -0,0 +1,29 @@
+package practice_sessions.DataTypes;
+
+public class DataTypesConcept {
+public static void main (String[] args){
+//without main method, we cannot execute the program
+
+    //type of data
+    //representation of the data
+
+    //1. Primitive Data Type : All are reserved Data Types
+    //Numeric Type:
+        //Integral Value
+            //Integer:: Byte, short, int, long
+            //Floating type:  float, double
+
+    //Character:c char $ a 1
+
+    //Boolean Type: boolean (True/False)
+
+
+    //2. Non Primitive data types : Class. interface, arrays , object
+
+    //1. byte:
+    byte b =10;
+    byte k =12;
+
+}
+
+}
---
+++ src/main/java/practice_sessions/Practice1.java
@@ -0,0 +1,16 @@
+package practice_sessions;
+
+public class Practice1 {
+
+    public static void main (String[] args){
+        System.out.println("Hello world" );
+        int temp = 2;
+        for (int i=1;i<=10;i++){
+            System.out.println(i);
+            int tem2p = temp +i;
+            temp ++;
+        }
+
+    }
+
+}
